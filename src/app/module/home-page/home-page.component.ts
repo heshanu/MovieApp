9de7e94b381cd97efcf9d82bbea34912378a7089
heshanu <heshanu97@gmail.com>
2023-12-25 05:48:18 +0530

    updated

--- src/app/module/home-page/home-page.component.ts
+++ src/app/module/home-page/home-page.component.ts
@@ -28,5 +28,7 @@ export class HomePageComponent {
     //console.log(movie);
   }
 
+  
+
 }
 
--- src/app/module/seatbooking/seatbooking.component.ts
+++ src/app/module/seatbooking/seatbooking.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { FetchService } from '../../service/fetch.service';
+import { Movie } from '../../shared/interface/movie';
+import { log } from 'node:console';
 
 @Component({
   selector: 'app-seatbooking',
@@ -6,5 +9,29 @@ import { Component } from '@angular/core';
   styleUrl: './seatbooking.component.css'
 })
 export class SeatbookingComponent {
+  movieList: Movie[] = [];
 
+  selectedMovieName: string = "Avatar";
+  
+  constructor(private fetchService: FetchService) { 
+    this.movieList = this.fetchService.movies; 
+  }
+  selectedMovieObj!:Movie;
+ 
+  public onChangeAcccordingFilmName(movie:Movie):void {
+    console.log(`this is two way binding`, this.selectedMovieName);
+    alert(this.selectedMovieName);
+
+    // const movieData = this.movieList.find((movie: Movie) => { 
+    //   alert($event.target.value);
+    //   return movie.Title=== $event.target.value;
+    // })  
+    // alert(movieData);
+
+    // if(movieData) {
+    //   this.selectedMovieObj = movieData;
+    //   console.log(``,movieData);
+    // }
+        
+  } 
 }
